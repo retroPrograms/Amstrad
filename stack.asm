d70461b361a7ccb4f0dd3f02c6a8d295e185dc56
retroPrograms <mcarvalho_au@hotmail.com>
2021-02-16 22:44:34 +1100

    Added stack.asm (2)

--- stack.asm
+++ stack.asm
@@ -34,6 +34,46 @@ Org &8000		;25:48
 	Call PrintChar
 ret
 
+Org &8100
+	ld hl,Introduction
+	call PrintString
+
+	call newline
+
+	ld hl,Message
+	call Printstring
+
+ret
+
+Printstring:
+	ld a,(hl)
+	cp 255
+	ret z
+	inc hl
+	call PrintChar
+jr Printstrin
+
+Introduction:
+	db 'Thought of the day...',255 ;string ends with 255
+
+Message:	db 'Z80 is Awesome!',255
+
+Newline:
+	ld a,13
+	call Printchar
+	ld a,10
+	jp printchar
+
+
+org &8200
+
+
+
+
+
+SquareBrackets: db '[]'
+CurlyBrackets: db '()'
+
 
 Temp: db 0
 Temp2: db 0
